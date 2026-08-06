35793b5f725ce1d528805298d224ccdd1fac6042
PRITHIV .S <prithiv16official@gmail.com>
2026-08-06 18:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (67.30%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int t) {
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            if(nums[m]==t)
+            {
+                return m;
+            
+            }
+            else if(nums[m]<t)
+            {
+                l=m+1;
+            }
+            else 
+            {
+                r=m-1;
+            }
+
+        }
+
+     return l;  
+    }
+}
